b3c20f1286ae5aa86313dd8709a5d1dff27536b5
JoSophie <105291138+JoSophie@users.noreply.github.com>
2022-05-14 17:45:47 +0300

    Initial_Commit

---
+++ Summator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Summator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Summator.Sum(new int[] { 1, 2, 3, 4, 5, 6, });
+            if (result == 21)
+            {
+                Console.WriteLine("TEST PASS");
+            } else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+ 
+        
+            long avrg = Summator.Average(new int[] { 5, 10, 15 });
+            if (avrg == 10)
+            {
+                Console.WriteLine("TEST PASS");
+            }   else
+            {
+                Console.WriteLine("TEST FAIL");
+            }
+        }
+    }
+}
